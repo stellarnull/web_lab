1954c053a6bc2d9ab687ab8e5d060a9ac395bfca
stellarnull <stellar@wlan-145-94-185-55.wlan.tudelft.nl>
2015-11-27 12:28:33 +0100

    css hello

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var i;
+var i; //global counter
 function list(task,due,priority,done){
 	
 	this.task=task;
@@ -21,26 +21,33 @@ list.prototype.get
 function updateDone(){
 	var me =this;
 	console.log(me);
+
+	//find the line whose checkbox is checked
 	var DoneID=this.id.replace("cb_","");
+
+	//find the corresponding sp_ID
 	var targetspan=document.getElementById("sp_"+DoneID);
 	console.log("sp_"+DoneID);
+
+	//done
 	targetspan.innerText="DONE!";
 	newlist[DoneID].done="DONE!";
 	console.log(newlist[DoneID].task);
 }
 
 function updateDelete(){
-	var me =this;
+	var me = this;
 	var j;
 	console.log(me);
-	var DeleteID=this.id.replace("db_","");
-	var targetList=document.getElementById("todo"+DeleteID);
+	var DeleteID = this.id.replace("db_","");
+	var targetList = document.getElementById("todo"+DeleteID);
 	console.log("DeleteID:"+DeleteID);
 	$(targetList).remove();
 	console.log(i);
-	for(j=DeleteID+1;j<i;j++)
+
+	for (j = DeleteID + 1; j < i; j++)
 	{
-		newlist[j]=newlist[j+1];
+		newlist[j] = newlist[j+1];
 		console.log(targetList.task);
 		console.log(document.getElementById("db_"+DeleteID).id);
 		document.getElementById("todo"+j).id="todo"+j-1;
@@ -59,58 +66,83 @@ var sort=function(){
 };
 
 var newlist=Array();
-var main=function(){
+var main=function()
+{
 	"use strict";
 	i=0;
-console.log("hello world!");
+	console.log("hello world!");
 	
-//this function is for adding new item to the list
-var addCommentFromInputBox = function () {		
-		console.log("New todo created!");
+	//this function is for adding new item to the list
+	var addCommentFromInputBox = function () 
+	{		
+		//find todolist tag
 		var List = document.getElementById("todolist");
+
+		//add a item
 		var new_TODO=document.createElement("li");
 		var span=document.createElement("span");
-			span.id="sp_"+i;
-			new_TODO.id="todo"+i;
-		 newlist[i]=new list($("#task").val(), $("#due").val(), 
-			$("#priority").val(),"Unfinished");
+		span.id="sp_"+i;
+		new_TODO.id="todo"+i;
+
+		//create content
+		newlist[i]=new list($("#task").val(), $("#due").val(), $("#priority").val(),"Unfinished");
+
+		//declare buttons
 		var Donebox=document.createElement("input");
-			Donebox.type="checkbox";
-			Donebox.onclick=updateDone;
-			Donebox.id="cb_"+i;
+		Donebox.type="checkbox";
+		Donebox.onclick=updateDone;
+		Donebox.id="cb_"+i;
 		var Deletebutton=document.createElement("input");
-			Deletebutton.type="button";
-			Deletebutton.Text="Delete!";
-			Deletebutton.onclick=updateDelete;
-			Deletebutton.id="db_"+i;
-			//to ensure only valid input are typed
+		Deletebutton.type="button";
+		Deletebutton.Text="Delete!";
+		Deletebutton.onclick=updateDelete;
+		Deletebutton.id="db_"+i;
+
+		//to ensure only valid input are typed
 		if ($("#task").val()===""||$("#due").val()===""||!($("#priority").val()==='1'||$("#priority").val()==='2'||$("#priority").val()==='3')) 
+		{
+			console.log("invalid input!");
 			window.alert("invalid input!");
-		else{console.log("it's fine!");
-		span.innerText=newlist[i].getAList();
-		new_TODO.appendChild(span);
-		new_TODO.appendChild(Donebox);
-		new_TODO.appendChild(Deletebutton);
-		List.appendChild(new_TODO);
-		//var setDoneButton= $('<button id="DoneB"+i>Unfinished</button>');
-		//$(".TODOs").append(newlist.Donebox);
-		//var DeleteButton= $('<button id="DeleteB"+i>Delete</button>');
-		//$(".TODOs").append(newlist.Deletebutton);
-		$("#task").val("");$("#due").val("");
-		$("#priority").val("");
-		console.log(i);
-		i=i+1;
-	}
+		}
+		else
+		{
+			console.log("New todo created!");
+			span.innerText=newlist[i].getAList();
+
+			//add the new item
+			new_TODO.appendChild(span);
+			new_TODO.appendChild(Donebox);
+			new_TODO.appendChild(Deletebutton);
+			List.appendChild(new_TODO);
+
+			//var setDoneButton= $('<button id="DoneB"+i>Unfinished</button>');
+			//$(".TODOs").append(newlist.Donebox);
+			//var DeleteButton= $('<button id="DeleteB"+i>Delete</button>');
+			//$(".TODOs").append(newlist.Deletebutton);
+			$("#task").val("");$("#due").val("");
+			$("#priority").val("");
+			console.log(i);
+
+			//increment counter
+			i = i + 1;
+		}
 	};
 
-//Either Enter or click the button will trigger the addCommentFromTnputBox() function
-$(".TODO-input button").on("click",function (event){
-	addCommentFromInputBox();
-});
-$(".TODO-input input").on("keypress", function (event) {
-if (event.keyCode === 13) {addCommentFromInputBox();}
-});
+	//Either Enter or click the button will trigger the addCommentFromTnputBox() function
+	$(".TODO-input button").on("click", function (event)
+	{
+		addCommentFromInputBox();
+	});
+
+	$(".TODO-input input").on("keypress", function (event) 
+	{
+		if (event.keyCode === 13) 
+		{
+			addCommentFromInputBox();
+		}
+	});
 };
+
 $(document).ready(main);
 
 
