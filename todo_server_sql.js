bbd2b976b3f49f31b0df9ddd938b8ffeb3d35904
Stellar Chan <869337858@qq.com>
2015-12-13 20:18:10 +0100

    sql through url

--- todo_server_sql.js
+++ todo_server_sql.js
@@ -10,7 +10,7 @@ var connection = mysql.createConnection({
 	"hostname": "localhost",
 	"user": "root",
 	"password": "webdata",
-	"database": "dump"
+	"database": "todo"
 });
 connection.connect();
 
@@ -22,15 +22,14 @@ http.createServer(app).listen(port);
 
 //clients requests todos
 //?q=select * from ToDoList
-app.get("/", function (req, res) 
+app.get("/todos", function (req, res) 
 {
-	//var url_parts = url.parse(req.url, true);
-	//var query = url_parts.query;
-	var query = "select * from ToDoList";
-	console.log(query);
+	var query = "select * from ToDoItem";
+	//console.log(query.q);
 
 	var t = "Retrieved from database: ";
-
+	
+	console.log("todos requested!");
 	// Execute query
 	connection.query(query, function(e, rows) 
 	{
@@ -40,12 +39,103 @@ app.get("/", function (req, res)
 		} 
 		else 
 		{
-			t += JSON.stringify(rows);
+			var s = JSON.stringify(rows);
+			t += s;
 		}
 		t += "\n";
+		res.write(t);
+		res.end();
 	});
-	console.log("todos requested!");
-	res.write(t);
+});
+
+//addtodo?task=maths&pri=3&due=123
+app.get("/addtodo", function (req, res) 
+{
+	var url_parts = url.parse(req.url, true);
+	var query = url_parts.query;
+	var task = query.task;
+	var pri = query.pri;
+	var q = "INSERT INTO ToDoItem(text, Priority) VALUES ('"+task+"', "+pri+");";
+	console.log(q);
+	var t = "Retrieved from database: ";
+	
+	console.log("todos added!");
+	// Execute query
+	connection.query(q, function(e, rows) 
+	{
+		if (e) 
+		{
+			throw e;
+		} 
+		else 
+		{
+			var s = JSON.stringify(rows);
+			t += s;
+		}
+		t += "\n";
+		res.write(t);
+		res.end();
+	});
+});
+
+//detodo?id=30
+app.get("/detodo", function (req, res) 
+{
+	var url_parts = url.parse(req.url, true);
+	var query = url_parts.query;
+	var id = query.id;
+	var q = "DELETE FROM ToDoItem where Id="+id+";";
+	console.log(q);
+	var t = "Retrieved from database: ";
+	
+	console.log("todos deleted!");
+	// Execute query
+	connection.query(q, function(e, rows) 
+	{
+		if (e) 
+		{
+			throw e;
+		} 
+		else 
+		{
+			var s = JSON.stringify(rows);
+			t += s;
+		}
+		t += "\n";
+		res.write(t);
+		res.end();
+	});
+});
+
+//uptodo?id=1&task=maths&pri=3&due=123
+app.get("/uptodo", function (req, res) 
+{
+	var url_parts = url.parse(req.url, true);
+	var query = url_parts.query;
+	var id = query.id;
+	var task = query.task;
+	var pri = query.pri;
+	var q = "update ToDoItem set text='"+task+"',Priority="+pri+" where Id="+id+";";
+	console.log(q);
+	var t = "Retrieved from database: ";
+	
+	console.log("todos updated!");
+	// Execute query
+	connection.query(q, function(e, rows) 
+	{
+		if (e) 
+		{
+			throw e;
+		} 
+		else 
+		{
+			var s = JSON.stringify(rows);
+			t += s;
+		}
+		t += "\n";
+		res.write(t);
+		res.end();
+	});
 });
 
 // Notify that the server is now running
